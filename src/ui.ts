33a1a4423c335eee49b8a049c3176a5aa329f146
soso <ethdud1@gmail.com>
2020-07-05 16:18:12 +0900

    feat: add proto type

--- src/ui.ts
+++ src/ui.ts
@@ -5,8 +5,16 @@ export function apply() {
     const diffStat =
       Number($("#diffstat .text-green").text().trim().replace(/,/g, ''))
       + Math.abs(Number($("#diffstat .text-red").text().trim().replace(/,/g, '').replace('−','')));
-    const packageLockDiff = $("a:contains(package-lock.json)").text();
+    const packageLockDiff = $("div[data-path='package-lock.json'] span.diffstat").text().replace(/,/g, '')
     const realDiff = diffStat - Number(packageLockDiff);
     console.log('@@ realDiff', realDiff)
+    $("#diffstat").parent().append(`
+      <span style='
+      color: #2468A5;
+      font-weight: bold;
+      '>
+        realDiff: ${realDiff}
+      </span>`
+    )
   }, 2000)
 }
