2f57afee8235221c6a216440ac5e9be83f875cf1
soso <ethdud1@gmail.com>
2020-07-05 18:35:58 +0900

    feat: add token saga

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import { all } from 'redux-saga/effects'
 
 import { PR_SLICE,prReducer } from './prSlice'
+import { watchSaveUserToken } from './tokenSaga'
 import { USER_INFO,userInfoReducer } from './tokenSlice'
 
 const rootReducer = combineReducers({
@@ -9,7 +12,17 @@ const rootReducer = combineReducers({
   [USER_INFO]: userInfoReducer,
 })
 
-const store = configureStore({ reducer: rootReducer })
+export const sagaMiddleware = createSagaMiddleware()
+export function* rootSaga() {
+  yield all([
+    watchSaveUserToken(),
+  ])
+}
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+})
 
 export type RootState = ReturnType<typeof rootReducer>
 export default store
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import App from '@/components/App'
-import store from '@/features'
+import store, { rootSaga,sagaMiddleware } from '@/features'
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
   <Provider store={store}>
