772221d8d0e6dc4f9dc5ea6dad059bdd74129bcc
soso <ethdud1@gmail.com>
2020-07-05 17:50:21 +0900

    chore: remove history util

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router } from 'react-router-dom'
 
 import App from '@/components/App'
 import store from '@/features'
-import history from '@/utils/history'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root'),
 )
