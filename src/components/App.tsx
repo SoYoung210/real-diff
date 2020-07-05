9e721d51c1a9b731505497b29e1c7578bfb2b92a
soso <ethdud1@gmail.com>
2020-07-05 18:14:41 +0900

    style: fix popup layout style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,32 @@
 import styled from '@emotion/styled'
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Layout = styled.div`
   margin: 10px;
+  width: 280px;
 `
 const Head = styled.h1`
   font-weight: 700;
   margin: 10px 0;
 `
-const TextInputField = styled.input``
+const TextInputField = styled.input`
+  display: block;
+  width: 100%;
+`
+const SaveButton = styled.button`
+  padding: 8px 0px;
+  margin-top: 10px;
+  width: 100%;
+  background: rgba(24,205,140,0.1);
+  border-radius: 8px;
+  color: #18CD8C;
+  font-weight: bold;
+  font-size: 14px;
+`
 // https://docs.github.com/en/rest/reference/pulls#list-pull-requests-files
 export default () => {
+  const dispatch = useDispatch()
   const [token, setToken] = useState('')
 
   return (
@@ -22,6 +38,7 @@ export default () => {
           setToken(target.value)
         }
       />
+      <SaveButton>Save</SaveButton>
     </Layout>
   )
 }
