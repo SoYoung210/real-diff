198a87a247193aae290b36708fbeab5e56becf5f
SoYoung210 <ethdud1@gmail.com>
2020-07-06 13:28:41 +0900

    feat: add api functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosInstance } from 'axios'
 export enum FetchStatusCode {
   DEFAULT = -1,
   LOADING = 99,
@@ -6,4 +7,27 @@ export enum FetchStatusCode {
   UNAUTHORIZED = 401,
   UNKNOWN = 500,
   NOT_FOUND = 404,
+}
+
+const axiosInstance = (token: string): AxiosInstance => {
+  const headers = {
+    Accept: 'application/json',
+    Pragma: 'no-cache',
+    Authorization: token,
+  }
+
+  return axios.create({
+    baseURL: 'https://api.github.com',
+    headers,
+  })
+}
+
+export const pullRequestAPI = async (token: string , url: string) => {
+  try {
+    const { data } = await axiosInstance(token).get(url)
+
+    return data
+  } catch (error) {
+    throw error
+  }
 }
