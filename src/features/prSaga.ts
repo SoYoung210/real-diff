d494f12f85dfd2aa522d277f1bae8b42394dd165
SoYoung210 <ethdud1@gmail.com>
2020-07-09 13:30:08 +0900

    feat: add github pr api

--- src/features/prSaga.ts
+++ src/features/prSaga.ts
@@ -1,6 +1,7 @@
 import { PayloadAction } from '@reduxjs/toolkit'
-import { select, takeLatest } from 'redux-saga/effects'
+import { call,select, takeLatest } from 'redux-saga/effects'
 
+import { pullRequestAPI } from '@/api'
 import { tokenSelector } from '@/features/settingSlice'
 
 import { prActions } from './prSlice'
@@ -9,14 +10,26 @@ function* fetchPullRequestFiles({payload}: PayloadAction<string>) {
   // ['', 'SoYoung210', 'real-diff', 'pull', '1']
   const pathName = payload.split('/')
   // TODO: Refactor
+  // const parsedPathName = {
+  //   orgName: pathName[1],
+  //   repository: pathName[2],
+  //   prNumber: pathName[4],
+  // }
+  // FIXME: Test 환경 값에 따라 목데이터 분리하기
   const parsedPathName = {
-    orgName: pathName[1],
-    repository: pathName[2],
-    prNumber: pathName[4],
+    orgName: 'SoYoung210',
+    repository: 'real-diff',
+    prNumber: 1,
   }
+  const { orgName, repository, prNumber } = parsedPathName
   const { token } = yield select(tokenSelector.token)
-  console.log('parsedPathName')
 
+  const data = yield call(
+    pullRequestAPI,
+    token,
+    `/repos/${orgName}/${repository}/pulls/${prNumber}/files`,
+  )
+  console.log('@@ Data', data)
 }
 
 export function* watchFetchPullRequest() {
