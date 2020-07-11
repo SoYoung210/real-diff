81f2347ebf4e7974808689a4be3fda614cd8ab88
soso <ethdud1@gmail.com>
2020-07-11 18:25:21 +0900

    test: add filterIgnoredFiles test case

--- src/utils/ignoredFileFilter.ts
+++ src/utils/ignoredFileFilter.ts
@@ -1,7 +1,13 @@
+const getLastItem = <T>(arr: T[]) => {
+  return arr[arr.length - 1]
+}
+
+const getFileName = (fileNameWithPath: string) => {
+  return getLastItem(fileNameWithPath.split('/'))
+}
+
 export const filterIgnoredFiles = (ignoredList: string[]) => (
   filename: string,
 ) => {
-  return ignoredList.some(ignoredFile => (
-    ignoredFile.includes(filename)
-  ))
+  return ignoredList.includes(getFileName(filename))
 }
