2d2c0e657319746a4e0c97b5ae491c1e06c0c2ba
SoYoung210 <ethdud1@gmail.com>
2020-07-31 22:56:17 +0900

    style: add token view style

--- src/components/settings/Token.tsx
+++ src/components/settings/Token.tsx
@@ -1,15 +1,53 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import styled from '@emotion/styled'
+import React, { CSSProperties,useEffect,useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Button } from '@/components/shared/Button'
+import { ContentWrapper } from '@/components/shared/ContentWrapper'
 import { Input } from '@/components/shared/Input'
 import { InputContainer } from '@/components/shared/SettingInputContainer'
-import { settingActions } from '@/features/settingSlice'
+import { settingActions, settingSelector } from '@/features/settingSlice'
+
+const Content = styled.p`
+  color: #8B95A1;
+  height: 100%;
+  font-size: 14px;
+  line-height: 22px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  & > *:not(:last-child) {
+    margin-bottom: 4px;
+  }
+`
+
+const StyledLink = styled.a`
+  color: #2f5fd2;
+  padding-bottom: 1px;
+  border-bottom: 1px solid #2f5fd2;
+  font-size: 10px;
+  line-height: 12px;
+`
+const BoldText = styled.div`
+  font-weight: 900;
+`
+
+const SmallText = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+`
 
 // https://docs.github.com/en/rest/reference/pulls#list-pull-requests-files
 export const TokenSettingView = () => {
   const dispatch = useDispatch()
   const [token, setToken] = useState('')
+  const isTokenExist = useSelector(settingSelector.isTokenExist)
+
+  useEffect(() => {
+    dispatch(settingActions.checkTokenExist())
+  }, [dispatch])
 
   const saveToken = () => {
     dispatch(settingActions.saveToken(token))
@@ -17,10 +55,40 @@ export const TokenSettingView = () => {
 
   return (
     <>
+      <ContentWrapper offsetTopHeight={46}>
+        <Content>
+          {
+            isTokenExist
+            ? (
+              <>
+                <BoldText>200 Token Exist!</BoldText>
+                <StyledLink
+                  href='https://chrome.google.com/webstore/detail/real-diff/noolkogacjfdckeeclgddpabknbnjacd?hl=ko&'
+                >
+                  Review Real Diff
+                </StyledLink>
+                <SmallText>:pray:</SmallText>
+              </>
+            )
+            : (
+              <>
+                <BoldText>404 NotFound</BoldText>
+                <StyledLink
+                  href='https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token'
+                  rel="nofollow"
+                >
+                  Please Add GitHub Auth Token
+                </StyledLink>
+                <SmallText>:sob:</SmallText>
+              </>
+            )
+          }
+        </Content>
+      </ContentWrapper>
       <InputContainer>
         <Input
           value={token}
-          placeholder='Input Your GitHub Token'
+          placeholder='Enter Your GitHub Token'
           onChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
             setToken(target.value)
           }
